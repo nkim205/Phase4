8207bad93920704eb04f6e145bb279965edb66de
Nathan Kim <kimnathan0324@gmail.com>
2025-11-21 18:03:35 -0500

    init tailwind + components

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './index.css'
+import Test from './components/Test'
+
+const Home = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+    return (
+        <div className="text-red-500 text-8xl font-bold text-center mt-[8rem]">
+            Welcome to the hospital DBMS.
+        </div>
+    )
+}
+
+export default Test;
